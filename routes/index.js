d6626e1c1134b85f2781f2f379d6e885d75cac05
gerold leeb <gerold.leeb@gmail.com>
2021-08-27 11:23:09 +0200

    Added Network information

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var os = require('os');
 var router = express.Router();
 
+var networkInterfaces = os.networkInterfaces();
+
+var firtIP = JSON.stringify(networkInterfaces[Object.keys(networkInterfaces)[0]]);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', ipaddress: firtIP });
 });
 
 module.exports = router;
